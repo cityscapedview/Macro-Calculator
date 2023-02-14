d3abd7269870186bb8e6e0b41a07fb8c1fa2a1c9
David Fagan <davida.fagan@gmail.com>
2023-02-13 18:11:19 -0800

    Added functionality to macro caclulator
    Utilized formData to grab values from form and then wrote functions to calculate rest weight, goal-oriented weight, and macros.
    
    The functionality will log to console.  I will add js injection to display values in new html section.

--- script.js
+++ script.js
@@ -1,32 +1,71 @@
-// MACRO CALCULATOR L33T CODE HERE
-let userWeight = 155;
-let userGender = ;
-let userWeightGoal = ;
+const form = document.querySelector("form");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const fd = new FormData(form);
+  const userWeight = fd.get("weight");
+  const userGender = fd.get("gender");
+  const userGoal = fd.get("goal");
+  const userHeight = fd.get("height");
+
+  let x = restGoal(userWeight, userHeight, userGender, 33);
+  console.log(x);
+
+  let y = calGoal(x, userGoal);
+  console.log(y);
+
+  let z = macroCal(userWeight, x);
+  console.log(z);
+});
+
+// Function Calorie Goal
 // User inserts bodyweight in lbs  multiply it by 15 = maintenance weight
-let mainCal = userWeight * 15;
+// let mainCal = userWeight * 15;
 // reduce weight by 250-500 cals
 // increase weight by 250-500 cals
 // This might be better as a switch statement: case 1 weight gain case 2 weight loss case 3 maintenance
-if (userWeight = true) {
-  return mainCal -= 250;
-} else if (userWeight = false) {
-  return mainCal += 250;
-} else {
-  return mainCal;
+
+function restGoal(weight, height, gender, age) {
+  weight /= 2.205;
+  height *= 2.54;
+  let restCalories = 0;
+  if (gender == "male") {
+    return (restCalories = 10 * weight + 6.25 * height - 5 * age + 5) * 1.2;
+  } else {
+    return (restCalories = 10 * weight + 6.25 * height - 5 * age - 161) * 1.2;
+  }
+}
+
+function calGoal(rCal, goal) {
+  let mainCal = rCal;
+  if (goal == "gain") {
+    return (mainCal *= 1.15);
+  } else if (goal == "lose") {
+    return (mainCal *= 0.8);
+  } else {
+    return mainCal;
+  }
 }
 
 // protein= 4 calories per gram, fat= 9 calories per gram,  carb= 4 calories per gram
-// Protein: 1g per body weight.  
-let proteinGoal = userWeight;
-let macroCal = proteinGoal * 4;
+// Protein: 1g per body weight.
+// let proteinGoal = userWeight;
+// let macroCal = proteinGoal * 4;
 // Fat: minimun of 10-25g per day.
-let fatGoal = .03 x userWeight;
-macroCal += (9 x fatGoal);
+// let fatGoal = .03 x userWeight;
+// macroCal += (9 x fatGoal);
 // Carbs: 1-3g per gram.  Calculate fat and protein and then remainder can be carbs.
-let carbGoal = (mainCal - macroCal) / 4;
+// let carbGoal = (mainCal - macroCal) / 4;
 
-
-// To Do List: 
-// Find a way to make buttons generate user Input.
-// Find a way to have javascrip present calculations on website page.
+function macroCal(weight, mainCal) {
+  let protGoal = weight;
+  let fatGoal = weight * 0.4;
+  mainCal -= protGoal * 4 + fatGoal * 9;
+  let carbGoal = Math.floor(mainCal / 4);
+  return {
+    protGoal,
+    fatGoal,
+    carbGoal,
+  };
+}
