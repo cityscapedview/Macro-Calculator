c45e353fd199cbdc17194fc95b823bf389ed1599
David <Davida.fagan@gmail.com>
2023-03-19 16:09:27 -0700

    Global Container Section Added
    Global Container Section Added.
    
    Removed default files from vite setup.

--- MacroCalculator/src/App.jsx
+++ MacroCalculator/src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from "react";
 import Body from "./components/Body";
-
-//change below css file
-import "./App.css";
+import GlobalContainer from "./components/GlobalContainer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <Body />
+      <Body>
+        <GlobalContainer></GlobalContainer>
+      </Body>
     </div>
   );
 }
--- MacroCalculator/src/components/Body.jsx
+++ MacroCalculator/src/components/Body.jsx
@@ -1,7 +1,7 @@
 const Body = () => {
   return (
-    <div className="text-[#343a40] text-yellow-900 bg-slate-100 dark:text-white dark:bg-slate-900">
-      <p>Cat in Hat website</p>
+    <div className="text-[#343a40] bg-slate-100 dark:text-white dark:bg-slate-900">
+      <p>Cat App</p>
     </div>
   );
 };
