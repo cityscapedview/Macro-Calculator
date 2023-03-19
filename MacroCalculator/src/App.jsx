379329af22b4c764fce1c1b147073088bc95ff3c
David <Davida.fagan@gmail.com>
2023-03-19 16:56:58 -0700

    Instructions Section
    +Instructions Section Added
    
    I might break this down into further components in the future.

--- MacroCalculator/src/App.jsx
+++ MacroCalculator/src/App.jsx
@@ -3,6 +3,7 @@ import Body from "./components/Body";
 import GlobalContainer from "./components/GlobalContainer";
 import Header from "./components/Header";
 import HealthBenefits from "./components/HealthBenefits";
+import Instructions from "./components/Instructions";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <GlobalContainer>
           <Header />
           <HealthBenefits />
+          <Instructions />
         </GlobalContainer>
       </Body>
     </div>
